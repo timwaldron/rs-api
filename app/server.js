cac7600632414480947f737c9d9cc18390c77b31
Timothy Waldron <tim@waldron.im>
2019-06-17 07:58:53 +1000

    Removed old code commented out and unused vars

--- app/server.js
+++ app/server.js
@@ -3,9 +3,6 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-
-const PORT = process.env.PORT || 3000;
-
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
--- public/javascript/tabview.js
+++ public/javascript/tabview.js
@@ -13,23 +13,3 @@ const changeExample = (event, elementName) => {
   tabTarget.style.display = "block";
   // event.currentTarget.className += " active";
 }
-
-
-
-// function openCity(evt, cityName) {
-//   var i, tabcontent, tablinks;
-//   tabcontent = document.getElementsByClassName("tabcontent");
-
-//   for (i = 0; i < tabcontent.length; i++) {
-//     tabcontent[i].style.display = "none";
-//   }
-
-//   tablinks = document.getElementsByClassName("tablinks");
-
-//   for (i = 0; i < tablinks.length; i++) { 
-//     tablinks[i].className = tablinks[i].className.replace(" active", "");
-//   }
-
-//   document.getElementById(cityName).style.display = "block";
-//   evt.currentTarget.className += " active";
-// }
